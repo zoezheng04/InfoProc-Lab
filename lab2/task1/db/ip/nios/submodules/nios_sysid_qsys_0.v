7df5df790fa009a9fa06fa7bade7b58fb960bd2c
Zheng <137006967+zoezheng04@users.noreply.github.com>
2024-02-06 17:09:00 +0000

    lab2task1 updated

--- lab2/task1/db/ip/nios/submodules/nios_sysid_qsys_0.v
+++ lab2/task1/db/ip/nios/submodules/nios_sysid_qsys_0.v
@@ -49,7 +49,7 @@ module nios_sysid_qsys_0 (
 
   wire    [ 31: 0] readdata;
   //control_slave, which is an e_avalon_slave
-  assign readdata = address ? 1707045249 : 0;
+  assign readdata = address ? 1707238663 : 0;
 
 endmodule
 
