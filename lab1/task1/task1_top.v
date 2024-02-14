e0a7f43923456f6b905840c709a31b830057aad4
zoezheng04 <137006967+zoezheng04@users.noreply.github.com>
2024-02-14 02:34:49 +0000

    Delete lab1 directory

--- lab1/task1/task1_top.v
+++
@@ -1,9 +0,0 @@
-module task1_top (
-	SW, //input switches
-	HEX0 //Hex output on 7 segment display
-);
-	input [3:0] SW; //declare input output ports
-	output [6:0] HEX0; 
-	
-	hex_to_7seg SEG0 (HEX0, SW[3:0]);
-endmodule
